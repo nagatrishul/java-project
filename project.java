e5dfdd60ecbaad9363a0af17d9d9d71ff7c26e96
nagatrishul <regandlanagatrishul@gmail.com>
2026-02-23 23:41:01 +0530

    first file

---
+++ project.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class project{
+
+    public double handleRainyMonth(Scanner sc, double totalExpenses) {
+
+        System.out.print("Is it Rainy Month? (true/false): ");
+        boolean rainy = sc.nextBoolean();
+
+        if (rainy) {
+            System.out.print("Enter Extra Water Cost: ");
+            double water = sc.nextDouble();
+
+            System.out.print("Enter Repair Cost: ");
+            double repair = sc.nextDouble();
+
+            totalExpenses += (water + repair);
+        }
+
+        return totalExpenses;
+    }
+
+    public void validateExpenses(double income, double totalExpenses) {
+
+        if (totalExpenses > income) {
+            System.out.println("Warning: Expenses exceed Income!");
+        }
+    }
+}
